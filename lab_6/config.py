dd48d0147ade67d235867334968598a98ab664c2
Jupiter0303 <165270679+Jupiter0303@users.noreply.github.com>
2025-12-19 20:45:59 +0400

    исправил проблему обращения к config из разных директорий

--- lab_6/config.py
+++ lab_6/config.py
@@ -1,13 +1,16 @@
 import json
 
 
-with open ("settings.json", 'r') as js:
-    config = json.load(js)
+try:
+    with open("settings.json", 'r') as js:
+        config = json.load(js)
+except FileNotFoundError:
+    with open("lab_6/settings.json", 'r') as js:
+        config = json.load(js)
 
-
-INITIAL_FILE = config["initial_file"]
-ENCRYPTED_FILE = config["encrypted_file"]
-DECRYPTED_FILE = config["decrypted_file"]
-SYMMETRIC_KEY = config["symmetric_key"]
-PUBLIC_PEM = config["public_key"]
-PRIVATE_PEM = config["secret_key"]
+INITIAL_FILE = "lab_6/" + config["initial_file"]
+ENCRYPTED_FILE = "lab_6/" + config["encrypted_file"]
+DECRYPTED_FILE = "lab_6/" + config["decrypted_file"]
+SYMMETRIC_KEY = "lab_6/" + config["symmetric_key"]
+PUBLIC_PEM = "lab_6/" + config["public_key"]
+PRIVATE_PEM = "lab_6/" + config["secret_key"]
